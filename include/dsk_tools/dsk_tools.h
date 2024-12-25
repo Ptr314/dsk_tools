1e604bde2cd9fe4ab1bbba0675e0be301acb743d
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2024-12-25 15:28:16 +0300

    first commit

---
+++ include/dsk_tools/dsk_tools.h
@@ -0,0 +1,10 @@
+#ifndef DSK_TOOLS_H
+#define DSK_TOOLS_H
+
+namespace dsk_tools {
+
+    void hello();
+
+} // namespace
+
+#endif /* DSK_TOOLS_H */
---
+++ src/dsk_tools.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "dsk_tools/dsk_tools.h"
+
+namespace dsk_tools {
+
+void hello()
+{
+    std::cout << "Hello" << std::endl;
+}
+
+} // namespace
