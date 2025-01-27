d89581bfde9023ffd8da1234265530fbae9fb0de
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-01-27 19:34:26 +0300

    some work

--- include/dsk_tools/writer_hxc_mfm.h
+++ include/dsk_tools/writer_hxc_mfm.h
@@ -7,27 +7,27 @@ namespace dsk_tools {
 
 #pragma pack(push, 1)
 
-struct HXC_MFM_HEADER
-{
-    uint8_t  headername[7];          // "HXCMFM\0"
+    struct HXC_MFM_HEADER
+    {
+        uint8_t  headername[7];          // "HXCMFM\0"
 
-    uint16_t number_of_track;
-    uint8_t  number_of_side;         // Number of elements in the MFMTRACKIMG array : number_of_track * number_of_side
+        uint16_t number_of_track;
+        uint8_t  number_of_side;         // Number of elements in the MFMTRACKIMG array : number_of_track * number_of_side
 
-    uint16_t floppyRPM;              // Rotation per minute.
-    uint16_t floppyBitRate;          // 250 = 250Kbits/s, 300 = 300Kbits/s...
-    uint8_t  floppyiftype;
+        uint16_t floppyRPM;              // Rotation per minute.
+        uint16_t floppyBitRate;          // 250 = 250Kbits/s, 300 = 300Kbits/s...
+        uint8_t  floppyiftype;
 
-    uint32_t mfmtracklistoffset;    // Offset of the MFMTRACKIMG array from the beginning of the file in number of uint8_ts.
-};
+        uint32_t mfmtracklistoffset;    // Offset of the MFMTRACKIMG array from the beginning of the file in number of uint8_ts.
+    };
 
-struct HXC_MFM_TRACK_INFO
-{
-    uint16_t track_number;
-    uint8_t  side_number;
-    uint32_t mfmtracksize;          // MFM/FM Track size in bytes
-    uint32_t mfmtrackoffset;        // Offset of the track data from the beginning of the file in number of bytes.
-};
+    struct HXC_MFM_TRACK_INFO
+    {
+        uint16_t track_number;
+        uint8_t  side_number;
+        uint32_t mfmtracksize;          // MFM/FM Track size in bytes
+        uint32_t mfmtrackoffset;        // Offset of the track data from the beginning of the file in number of bytes.
+    };
 
 #pragma pack(pop)
 
--- src/image_agat840.cpp
+++ src/image_agat840.cpp
@@ -5,12 +5,12 @@ namespace dsk_tools {
     imageAgat840::imageAgat840(Loader * loader):
         imageAgat140(loader)
     {
-        format_heads = 1;
-        format_tracks = 160;
+        format_heads = 2;
+        format_tracks = 80;
         format_sectors = 21;
         format_bitrate = 250;
         format_rpm = 300;
-        format_track_encoding = UNKNOWN_ENCODING;
+        format_track_encoding = ISOIBM_MFM_ENCODING;
         format_floppyinterfacemode = GENERIC_SHUGGART_DD_FLOPPYMODE;
     }
 
@@ -23,5 +23,11 @@ namespace dsk_tools {
     {
         return sector;
     }
+
+    uint8_t * imageAgat840::get_sector_data(int head, int track, int sector)
+    {
+        return get_raw_sector_data(track & 1, track >> 1, sector);
+    }
+
 
 }
