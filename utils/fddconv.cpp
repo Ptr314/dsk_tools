874388c4bbecc19279672cf1e7d2f95dadc3a51d
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-01-29 22:21:15 +0300

    cli utils started

---
+++ utils/fddconv.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstdarg>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#include "cxxopts/cxxopts.hpp"
+#include "bail.hpp"
+
+void setupConsole() {
+#ifdef _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hOut, &mode);
+    SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+#endif
+}
+
+int main(int argc, char** argv)
+{
+    setupConsole();
+    try {
+        cxxopts::Options opts("fddconv", "FDD images conversion utility.");
+
+        opts.add_options()
+            //("v,verbose", "Вывод отладочной информации", cxxopts::value<bool>()->default_value("false"))
+            ("h,help", "Вывести справку")
+            ("input", "Входные файлы", cxxopts::value<std::vector<std::string>>());
+
+        opts.parse_positional({"input"});
+
+        auto res = opts.parse(argc, argv);
+        if (res["help"].as<bool>())
+        {
+            std::cout << opts.help() << std::endl;
+            return EXIT_SUCCESS;
+        }
+
+        if (res.count("input")) {
+            std::cout << "Позиционные аргументы:" << std::endl;
+            for (const auto& input : res["input"].as<std::vector<std::string>>()) {
+                std::cout << "  " << input << std::endl;
+            }
+        } else {
+            bail("No input file");
+        }
+
+    }
+    catch (const cxxopts::exceptions::exception& e)
+    {
+        return bail("Bad options: %s", e.what());
+    }
+}
