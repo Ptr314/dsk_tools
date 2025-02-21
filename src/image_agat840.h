9f95f5e4d40b837d88af12b8daf0d49fbc0e47f6
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-02-21 21:47:37 +0300

    memcpy & warnings

--- src/image_agat840.h
+++ src/image_agat840.h
@@ -11,7 +11,7 @@ namespace dsk_tools {
         imageAgat840(Loader * loader);
         virtual int translate_sector_logic2raw(int sector) override;
         virtual int translate_sector_raw2logic(int sector) override;
-        virtual uint8_t *get_sector_data(int head, int track, int sector);
+        virtual uint8_t *get_sector_data(int head, int track, int sector) override;
     };
 
 }
