f63fb7040e3f91cc1e6ed581982279cb81e7669a
Mikhail Revzin <p3.141592653589793238462643@gmail.com>
2025-03-31 09:31:41 +0300

    Agat BASIC

--- src/bas_tokens.h
+++ src/bas_tokens.h
@@ -21,6 +21,23 @@ namespace dsk_tools {
         "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?", "?",
         "?", "?", "?", "?", "?", "?"
     };
+
+    constexpr std::array<const char*, 128> Agat_tokens = {
+        "END", "FOR", "NEXT", "DATA", "INPUT", "DEL", "DIM", "READ", "SCREEN 1, ", "SCREEN 0, ",
+        "PR#", "IN#", "CALL", "LINE", "!", "&1", "SCREEN 1, ", "SCREEN 1, ", "COLOR", "LINE(",
+        "DRAW", "XDRAW", "LOCATE ,", "CLS", "ROT=", "SCALE=", "REM SHLOAD", "TRACE", "NOTRACE", "COLOR 7,0",
+        "COLOR 0,7", "COLOR 31,0", "COLOR", "POP", "LOCATE", "REM HIMEM:", "REM LOMEM:", "ON ERROR", "RESUME", "RECALL",
+        "STORE", "REM SPEED=", "LET", "GOTO", "RUN", "IF", "RESTORE", "&3", "GOSUB", "RETURN",
+        "REM", "STOP", "ON", "WAIT", "LOAD \"CON:", "SAVE \"CON:", "DEF", "REM POKE", "PRINT", "CONT",
+        "LIST", "CLEAR", "INPUT$(1)", "NEW", "TAB(", "TO", "FN", "SPC(", "THEN", "AT",
+        "NOT", "STEP", "+", "-", "*", "/", "^", "AND", "OR", ">",
+        "=", "<", "SGN", "INT", "ABS", "USR", "FRE", "POINT(", "PDL", "POS",
+        "SQR", "RND", "LOG", "EXP", "COS", "SIN", "TAN", "ATN", "PEEK", "LEN",
+        "STR$", "VAL", "ASC", "CHR$", "LEFT$", "RIGHT$", "MID$", " NO FOR", "SYNTAX", "NO GOSUB",
+        "NO DATA", "ILLEGAL VALUE", "OVERFLOW", "OUT OF MEMORY", "UNDEF STATEMENT", "SUBSCRIPT", "REDIM ARRAY", "DIVISION BY ZERO", "ILLEGAL DIRECT", "TYPE",
+        "LONG STRING", "BIG EXPR", "CONTINUE", "UNDEF NAME", "BYTE UNCOMPL", "LABEL", "OPCODE", "DOUBLE DEF NAME"
+    };
+
     constexpr std::array<const char*, 128> MBASIC_main_tokens = {
         "", "END", "FOR", "NEXT", "DATA", "INPUT", "DIM", "READ", "LET", "GOTO",
         "RUN", "IF", "RESTORE", "GOSUB", "RETURN", "REM", "STOP", "PRINT", "CLEAR", "LIST",
